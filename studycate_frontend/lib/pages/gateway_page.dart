9e4c46c931f2fc97ac1e323f0f31b76616bf6660
mihirgrand <mihirgrand@gmail.com>
2023-10-06 09:22:46 +0530

    shared prefs

---
+++ studycate_frontend/lib/pages/gateway_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GatewayPage extends StatefulWidget {
+  const GatewayPage({super.key});
+
+  @override
+  State<GatewayPage> createState() => _GatewayPageState();
+}
+
+class _GatewayPageState extends State<GatewayPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
